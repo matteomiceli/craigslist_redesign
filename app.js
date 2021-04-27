185683d348e76cf37fa147e70bdf294eef26d396
matteomiceli <omatteomiceli@gmail.com>
2021-04-27 03:22:08 -0700

    auth0 mostly working
    - time to integrate into routes, etc

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
+const { auth } = require('express-openid-connect');
 require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 8080;
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  baseURL: 'https://localhost:8080',
+  clientID: process.env.AUTH_CLIENT_ID,
+  issuerBaseURL: process.env.AUTH_DOMAIN,
+  secret: process.env.AUTH_CLIENT_SECRET
+};
+
+// Middleware
+app.use(auth(config));
+
 app.set('view engine', 'ejs');
 
+
 app.get('/', function (req, res) {
-  res.render('index')
+  res.render(req.oidc.isAuthenticated() ? 'index' : 'authError')
 })
+
 
 app.listen(port, () => {
     console.log('Listening on port 8080...')
